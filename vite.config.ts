fe551ddc7545db5ac1a16466be4bcc6a46c7f36b
luoqintai <luoqintai@weidian.com>
2023-03-22 17:08:51 +0800

    feat: init

--- vite.config.ts
+++ vite.config.ts
@@ -3,32 +3,15 @@ import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
-import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     outDir: '../dist/view',
-    rollupOptions: {
-      // output: {
-      //   assetFileNames: "assets/[name][extname]",
-      //   entryFileNames: "assets/[name].js"
-      // }
-    },
   },
   experimental: {
     renderBuiltUrl(filename: string, { hostId, hostType, type }: { hostId: string, hostType: 'js' | 'css' | 'html', type: 'public' | 'asset' }) {
-      console.log(filename,'????', typeof filename)
       return `./${filename}`;
-      if (type === 'public') {
-        return 'https://www.domain.com/' + filename
-      }
-      else if (path.extname(hostId) === '.js') {
-        return { runtime: `window.__assetsPath(${JSON.stringify(filename)})` }
-      }
-      else {
-        return 'https://cdn.domain.com/assets/' + filename
-      }
     }
   },
   plugins: [
